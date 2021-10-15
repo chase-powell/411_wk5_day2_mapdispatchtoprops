be0cdabdd63aeeee18a56fa12256c42938bafe17
chase-powell <landon.powel4@gmail.com>
2021-10-14 20:33:58 -0500

    final changes

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react'
+
+
 import {
     Button,
     TextField,
@@ -31,7 +33,9 @@ class AddCar extends Component {
         delete payload.open
         console.log("THE CAR", payload)
         // add this.props.addCar function here
+        this.props.addCar(payload)
         // also add this.setState to close the dialog
+        this.setState({ open: false })
     }
 
     componentDidUpdate = (prevProps, prevState) => {
